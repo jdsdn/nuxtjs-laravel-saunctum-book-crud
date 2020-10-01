2a9cf48c0d3b4d550af24cb916cdbf416d7d287b
xSeigefreid <silverice62@gmail.com>
2020-10-01 18:10:01 +0800

    Added Author Crud-Routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('register', 'API\RegisterController@register');
+Route::post('login', 'API\RegisterController@login');
+   
+Route::middleware('auth:api')->group( function () {
+    Route::resource('authors', 'API\AuthorController');
 });
